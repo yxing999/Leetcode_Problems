2cb35455e8ebc8dfb64259e0116c758b8d64ed02
yuxuan <yxing05@syr.edu>
2018-12-27 03:29:08 +0200

    adjust 959

---
+++ DFS/959.Regions_Cut_By_Slash(DFS).cpp
@@ -0,0 +1,12 @@
+/*
+Split every region into 4 parts:
+ __
+|\/|
+|/\|
+ --
+Make the corresponding relationship between parts and numbers:
+top:0,right:1,bottom:2,left:3.
+So, there are n*n*4 parts in the grid.
+
+*/
+
