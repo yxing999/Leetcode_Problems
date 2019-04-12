07fbe0db2349fc6e6fd299201bc3bccdaa953cbf
yuxuan <yxing05@syr.edu>
2019-04-12 00:44:51 -0400

    add 091

---
+++ Dynamic_Programming/091.Decode_Ways.py
@@ -0,0 +1,22 @@
+'''
+Take care of '0', '0' and '0x' is not allowed.
+
+Time: O(n),
+Space: O(n).
+'''
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if s[0] == '0':
+            return 0
+        n = len(s)
+        dp = [0 for i in range(n + 1)]
+        dp[0], dp[1] = 1, 1
+        
+        for i in range(2, n + 1) :
+            if s[i - 1] != '0' :
+                dp[i] = dp[i - 1]
+            if s[i - 2] == '1' or (s[i - 2] == '2' and s[i - 1] < '7') :
+                dp[i] += dp[i - 2]
+        
+        return dp[n]
