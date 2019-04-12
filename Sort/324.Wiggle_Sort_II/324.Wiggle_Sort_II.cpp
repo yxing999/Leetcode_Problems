a6b2fc95167902d34d68920a78697b70359a720e
yuxuan <37628246+yuxuan940905@users.noreply.github.com>
2019-04-11 23:27:46 -0400

    Update 324.Wiggle_Sort_II.cpp

--- Sort/324.Wiggle_Sort_II/324.Wiggle_Sort_II.cpp
+++ Sort/324.Wiggle_Sort_II/324.Wiggle_Sort_II.cpp
@@ -1,6 +1,6 @@
 /*
 It is not easy to handle equal values.
-We should try to construct new array pair by pair, with big pair at the front.
+We should try to construct new array pair by pair, with bigger pair at the front.
 For example: [0, 1, 2, | 3, 4, 5] -> [2, 5, 1, 4, 0, 3]
 if the length is odd, we should put the smallest one at end position.
 [0, | 1, 2, 3, | 4, 5, 6] -> [3, 6, 2, 5, 1, 4, 0].
