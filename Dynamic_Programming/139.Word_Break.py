c81ce9f3958a40550d7514e0c0adea7b1fd808f3
yuxuan <yxing05@syr.edu>
2019-05-24 21:20:26 +0800

    add 139 to dp

---
+++ Dynamic_Programming/139.Word_Break.py
@@ -0,0 +1,16 @@
+'''
+Time: O(n),
+Space: O(n).
+'''
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False for i in range(len(s) + 1)]
+        dp[0] = True
+        
+        for i in range(1, len(s) + 1) :
+            for j in range(i) :
+                if dp[j] and s[j:i] in wordDict :
+                    dp[i] = True
+                    break
+        
+        return dp[len(s)]
